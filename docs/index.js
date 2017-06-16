348c408984bcf9278c9dc06618b3e6c04e32fc44
Jetmate <antonoutkine@gmail.com>
2017-06-16 14:48:28 -0500

    general imporvements

--- docs/index.js
+++ docs/index.js
@@ -1,5 +1,5 @@
 (function() {
-  var animations, overlay, startAnimation;
+  var animations, element, end_animations, i, len, overlay, startAnimation;
 
   startAnimation = function(elements) {
     return function() {
@@ -20,5 +20,16 @@
   overlay.addEventListener('click', startAnimation(animations));
 
   overlay.addEventListener('touchstart', startAnimation(animations));
+
+  end_animations = document.getElementsByClassName('animation-end');
+
+  for (i = 0, len = end_animations.length; i < len; i++) {
+    element = end_animations[i];
+    element.addEventListener('animationend', function() {
+      if (element.parentElement != null) {
+        return element.parentElement.removeChild(element);
+      }
+    });
+  }
 
 }).call(this);
--- src/index.coffee
+++ src/index.coffee
@@ -7,3 +7,8 @@ animations = document.getElementsByClassName('animation')
 overlay = document.getElementsByClassName('overlay')[0]
 overlay.addEventListener 'click', startAnimation animations
 overlay.addEventListener 'touchstart', startAnimation animations
+end_animations = document.getElementsByClassName('animation-end')
+for element in end_animations
+  element.addEventListener 'animationend', ->
+    if element.parentElement?
+      element.parentElement.removeChild element
